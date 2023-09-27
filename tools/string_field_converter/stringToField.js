8b0db56e60f6a361e630679fa3e673422964b0e5
joengelh <73387330+joengelh@users.noreply.github.com>
2023-09-27 19:41:06 +0200

    add tool that validates if a string is too long or not

--- tools/string_field_converter/stringToField.js
+++ tools/string_field_converter/stringToField.js
@@ -12,6 +12,11 @@ function stringToBigInt(input) {
     bigIntValue = bigIntValue | shiftedValue;
   }
 
+  if (bigIntValue > 8444461749428370424248824938781546531375899335154063827935233455917409239040) {
+    console.log("\x1b[31m", "Field is too big and will not be correctly stored", "red");
+  } else {
+    console.log("\x1b[32m", "Field is small enough and will be correctly stored", "greed");
+  }
   return bigIntValue;
 }
 
